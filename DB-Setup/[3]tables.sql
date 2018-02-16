61421c2d6ef088d1f320fdfe7f360922609ae3bc
supercheng <chenghongyu628@gmail.com>
2018-02-16 14:40:18 -0500

    complete table definations

--- DB-Setup/[3]tables.sql
+++ DB-Setup/[3]tables.sql
@@ -22,7 +22,7 @@ CREATE TABLE `mmall_category` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `mmall_order` (
-	`id` int(11) not NULL AUTO_INCREMENT,
+	`id` int(11) NOT NULL AUTO_INCREMENT,
 	`order_no` bigint(20) DEFAULT NULL,
 	`user_id` int(11) DEFAULT NULL,
 	`shipping_id` Int(11) DEFAULT NULL,
@@ -39,3 +39,83 @@ CREATE TABLE `mmall_order` (
 	PRIMARY KEY (`id`),
 	UNIQUE KEY `order_no_index` (`order_no`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `mmall_order_item` (
+	`id` int(11) NOT NULL AUTO_INCREMENT,
+	`user_id` int(11) DEFAULT NULL,
+	`order_no` bigint(20) DEFAULT NULL,
+	`product_id` int(11) DEFAULT NULL,
+	`product_name` VARCHAR(100) DEFAULT NULL,
+	`product_image` VARCHAR(500) DEFAULT NULL,
+	`current_unit_price` decimal(20,2) DEFAULT NULL,
+	`quantity` int(10) DEFAULT NULL,
+	`total_price` decimal(20,2) DEFAULT NULL,
+	`create_time` datetime DEFAULT NULL,
+	`update_time` datetime DEFAULT NULL,
+	PRIMARY KEY (`id`),
+	KEY `order_no_index` (`order_no`) USING BTREE,
+	KEY `order_no_user_id_index` (`user_id`, `order_no`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `mmall_pay_info` (
+	`id` int(11) NOT NULL AUTO_INCREMENT,
+	`user_id` int(11) DEFAULT NULL,
+	`order_no` bigint(20) DEFAULT NULL,
+	`pay_platform` int(10) DEFAULT NULL,
+	`platform_number` VARCHAR(200) DEFAULT NULL,
+	`platform_status` VARCHAR(20) DEFAULT NULL,
+	`create_time` datetime DEFAULT NULL,
+	`update_time` datetime DEFAULT NULL,
+	PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `mmall_product` (
+	`id` int(11) NOT NULL AUTO_INCREMENT,
+	`category_id` int(11) NOT NULL,
+	`name` VARCHAR(100) NOT NULL,
+	`subtitle` VARCHAR(200) DEFAULT NULL,
+	`main_image` VARCHAR(500) DEFAULT NULL,
+	`sub_images` text,
+	`detail` text,
+	`price` decimal(20,2) NOT NULL,
+	`stock` int(11) NOT NULL,
+	`status` int(6) DEFAULT '1',
+	`create_time` datetime DEFAULT NULL,
+	`update_time` datetime DEFAULT NULL,
+	PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE `mmall_shipping` (
+	`id` int(11) NOT NULL AUTO_INCREMENT,
+	`user_id` int(11) DEFAULT NULL,
+	`receiver_name` VARCHAR(20) DEFAULT NULL,
+	`receiver_phone` VARCHAR(20) DEFAULT NULL,
+	`receiver_mobile` VARCHAR(20) DEFAULT NULL,
+	`receiver_province` VARCHAR(20) DEFAULT NULL,
+	`receiver_city` VARCHAR(20) DEFAULT NULL,
+	`receiver_district` VARCHAR(20) DEFAULT NULL,
+	`receiver_address` VARCHAR(200) DEFAULT NULL,
+	`receiver_zip` VARCHAR(6) DEFAULT NULL,
+	`create_time` datetime DEFAULT NULL,
+	`update_time` datetime DEFAULT NULL,
+	PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+CREATE TABLE  `mmall_user` (
+	`id` int(11) NULL AUTO_INCREMENT,
+	`username` VARCHAR(50) NOT NULL,
+	`password` VARCHAR(50) NOT NULL,
+	`email` VARCHAR(50) DEFAULT NULL,
+	`phone` VARCHAR(20) DEFAULT NULL,
+	`question` VARCHAR(100) DEFAULT NULL,
+	`answer` VARCHAR(100) DEFAULT NULL,
+	`role` int(4) NOT NULL COMMENT '0=Admin 1=User',
+	`create_time` datetime DEFAULT NULL,
+	`update_time` datetime DEFAULT NULL,
+	PRIMARY KEY (`id`),
+	UNIQUE KEY `user_name_unique` (`username`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
