69d5dfe63bf17cd0e0ebd4616e02cb1ceaa74091
superhcheng <chenghongyu628@gmail.com>
2018-02-17 21:39:18 -0500

    generate mybatis dao, pojo, and mapper xmls

---
+++ app-backend/src/main/java/us/supercheng/emall/dao/CartMapper.java
@@ -0,0 +1,17 @@
+package us.supercheng.emall.dao;
+
+import us.supercheng.emall.pojo.Cart;
+
+public interface CartMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Cart record);
+
+    int insertSelective(Cart record);
+
+    Cart selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Cart record);
+
+    int updateByPrimaryKey(Cart record);
+}
---
+++ app-backend/src/main/java/us/supercheng/emall/dao/OrderMapper.java
@@ -0,0 +1,17 @@
+package us.supercheng.emall.dao;
+
+import us.supercheng.emall.pojo.Order;
+
+public interface OrderMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Order record);
+
+    int insertSelective(Order record);
+
+    Order selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Order record);
+
+    int updateByPrimaryKey(Order record);
+}
