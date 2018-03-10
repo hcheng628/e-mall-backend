1cb02ac127bb4ebc12ecf4a234cab25db33e2f94
supercheng <chenghongyu628@gmail.com>
2018-03-10 10:43:30 -0500

    complete Category Module

--- app-backend/src/main/java/us/supercheng/emall/controller/backend/CategoryController.java
+++ app-backend/src/main/java/us/supercheng/emall/controller/backend/CategoryController.java
@@ -6,13 +6,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import us.supercheng.emall.common.Const;
+import us.supercheng.emall.common.ResponseCode;
 import us.supercheng.emall.common.ServerResponse;
 import us.supercheng.emall.pojo.Category;
 import us.supercheng.emall.pojo.User;
 import us.supercheng.emall.service.ICategoryService;
 import us.supercheng.emall.service.IUserService;
 import javax.servlet.http.HttpSession;
+import java.util.HashSet;
 import java.util.List;
 
 @Controller
@@ -29,33 +30,47 @@ public class CategoryController {
     @ResponseBody
     public ServerResponse getCategoriesByParentId(@RequestParam(defaultValue = "0") int parentId, HttpSession session) {
         User user = this.iUserService.getCurrentUser(session);
-        if (user == null) {
-            return ServerResponse.createServerResponseError("Login Required");
+        ServerResponse serverResponse = this.iUserService.checkAdminUser(user);
+        if (serverResponse.getStatus() == ResponseCode.SUCCESS.getCode()) {
+            ServerResponse<List<Category>> serverResponseCategoryList = this.iCategoryService.getCategoriesByParentId(parentId);
+            if (serverResponseCategoryList.getData().size() > 0) {
+                return serverResponseCategoryList;
+            }
+            return ServerResponse.createServerResponseError("No Such Category ID: " + parentId + " Found.");
         }
-        if (user.getRole() == Const.ROLE_ADMIN) {
-            return ServerResponse.createServerResponseError("Username: " + user.getUsername() + " is not an Admin User");
-        }
-
-        ServerResponse<List<Category>> serverResponse = this.iCategoryService.getCategoriesByParentId(parentId);
-
-        if(serverResponse.getData().size() > 0) {
-            return serverResponse;
-        }
-        return ServerResponse.createServerResponseError("No Such Category ID: " + parentId + " Found.");
+        return serverResponse;
     }
-
 
     @RequestMapping(value = "add_category.do", method = RequestMethod.POST)
     @ResponseBody
-    public ServerResponse addCategory(@RequestParam(defaultValue = "0") Integer parentId, String categoryNames, HttpSession session) {
+    public ServerResponse addCategory(@RequestParam(defaultValue = "0") Integer parentId, String categoryName, HttpSession session) {
         User user = this.iUserService.getCurrentUser(session);
-        if (user == null) {
-            return ServerResponse.createServerResponseError("Login Required");
+        ServerResponse serverResponse = this.iUserService.checkAdminUser(user);
+        if (serverResponse.getStatus() == ResponseCode.SUCCESS.getCode()) {
+            return this.iCategoryService.addCategory(parentId, categoryName);
         }
-        if (user.getRole() == Const.ROLE_ADMIN) {
-            return ServerResponse.createServerResponseError("Username: " + user.getUsername() + " is not an Admin User");
-        }
-        return this.iCategoryService.addCategory(parentId, categoryNames);
+        return serverResponse;
     }
 
+    @RequestMapping(value = "set_category_name.do", method = RequestMethod.POST)
+    @ResponseBody
+    public ServerResponse setCategoryName(Integer categoryId, String categoryName, HttpSession session) {
+        User user = this.iUserService.getCurrentUser(session);
+        ServerResponse serverResponse = this.iUserService.checkAdminUser(user);
+        if (serverResponse.getStatus() == ResponseCode.SUCCESS.getCode()) {
+            return this.iCategoryService.setCategoryName(categoryId, categoryName);
+        }
+        return serverResponse;
+    }
+
+    @RequestMapping(value = "get_deep_category.do", method = RequestMethod.GET)
+    @ResponseBody
+    public ServerResponse getDeepCategory(Integer categoryId, HttpSession session) {
+        User user = this.iUserService.getCurrentUser(session);
+        ServerResponse serverResponse = this.iUserService.checkAdminUser(user);
+        if (serverResponse.getStatus() == ResponseCode.SUCCESS.getCode()) {
+            return this.iCategoryService.getDeepCategory(categoryId, new HashSet<Category>());
+        }
+        return serverResponse;
+    }
 }
